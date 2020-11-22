19eb8dfc4526b4724ba65f1fb15da810c76ccd88
KimEoJin1 <kimuj7017@postech.ac.kr>
2020-11-22 16:56:49 +0900

    Code Churn(Fourth)

--- src/main/java/org/team4/team4_project/git/CodeChurn.java
+++ src/main/java/org/team4/team4_project/git/CodeChurn.java
@@ -2,16 +2,25 @@ package org.team4.team4_project.git;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.LogCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CodeChurn {
@@ -42,11 +51,12 @@ public class CodeChurn {
         return this;
     }
 
-    public ChurnResult calc() throws GitAPIException, IOException {
-        ChurnResult result = new ChurnResult();
-        Iterable<RevCommit> commits = log.call();
-        //for (RevCommit commit : commits) {
-        RevCommit commit = commits.iterator().next();
+    public List<ChurnResult> calc() throws GitAPIException, IOException {
+        List<ChurnResult> results = new ArrayList<ChurnResult>();
+        Iterable<RevCommit> commits = log.all().call();
+        for (RevCommit commit : commits) {
+        //RevCommit commit = commits.iterator().next();
+            ChurnResult result = new ChurnResult();
             RevCommit[] parents = commit.getParents();
             for (RevCommit parent : parents) {
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -57,7 +67,7 @@ public class CodeChurn {
                 formatter.format(parent, commit);
                 String difference = stream.toString();
                 Scanner scanner = new Scanner(difference);
-                System.out.println(difference);
+                //System.out.println(difference);
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
                     if (line.startsWith("+") && !line.startsWith("+++")) {
@@ -67,8 +77,40 @@ public class CodeChurn {
                     }
                 }
             }
-        //}
-        return result;
+            if (parents.length == 0){
+                ObjectReader reader = repo.newObjectReader();
+                RevWalk rw = new RevWalk(reader);
+                RevTree rtree = rw.parseTree(commit);
+                CanonicalTreeParser parser = new CanonicalTreeParser();
+                parser.reset(reader, rtree);
+                AbstractTreeIterator iterator = (AbstractTreeIterator)parser;
+                AbstractTreeIterator empty = iterator.createEmptyTreeIterator();
+                TreeWalk walk = new TreeWalk(reader);
+                walk.addTree(empty);
+                walk.addTree(iterator);
+                List<DiffEntry> diffs = DiffEntry.scan(walk);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                DiffFormatter formatter = new DiffFormatter(stream);
+                formatter.setRepository(repo);
+                formatter.setContext(0);
+                formatter.setPathFilter(PathFilter.create(path));
+                formatter.format(diffs);
+                String difference = stream.toString();
+                Scanner scanner = new Scanner(difference);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.startsWith("+") && !line.startsWith("+++")) {
+                        result.plusLinesAdded();
+                    } else if (line.startsWith("-") && !line.startsWith("---")) {
+                        result.plusLinesDeleted();
+                    }
+                }
+            }
+            results.add(result);
+        }
+
+
+        return results;
     }
 
 
--- src/test/java/org/team4/team4_project/git/GitCodeChurnTest.java
+++ src/test/java/org/team4/team4_project/git/GitCodeChurnTest.java
@@ -2,6 +2,8 @@ package org.team4.team4_project.git;
 
 import static org.junit.Assert.*;
 import java.io.File;
+import java.util.List;
+
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -9,15 +11,14 @@ import org.junit.Test;
 
 public class GitCodeChurnTest {
 
-    public static final String testFilePath = "datatest/homework0";
+    public static final File testfile = new File("datatest/homework0/.git");
 
     @Test
     public void CodeChurnTest() throws Exception{
-        GitHandler gitHandler = new GitHandler(testFilePath);
-        Repository repo = gitHandler.getRepository();
-        ChurnResult result = new CodeChurn(repo).addPath(".gitignore").calc();
-        assertEquals(1,result.getLinesAdded());
-        assertEquals(1,result.getLinesDeleted());
+        Repository repo = new FileRepositoryBuilder().setGitDir(testfile).readEnvironment().findGitDir().build();
+        List<ChurnResult> result = new CodeChurn(repo).addPath("README.md").calc();
+        assertEquals(5,result.get(result.size()-1).getLinesAdded());
+        //assertEquals(1,result.getLinesDeleted());
     }
 
 }
