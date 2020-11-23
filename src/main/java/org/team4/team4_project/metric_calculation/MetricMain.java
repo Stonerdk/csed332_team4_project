5ff0afdefa28ca6fb8f3892321c2d95478dd5aaf
chaeyoon17 <chaeyoon17@postech.ac.kr>
2020-11-24 00:23:44 +0900

    Add MetricMain class (class used to parse source code)

---
+++ src/main/java/org/team4/team4_project/metric_calculation/MetricMain.java
@@ -0,0 +1,13 @@
+package org.team4.team4_project.metric_calculation;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.core.dom.*;
+
+
+public class MetricMain {
+
+}
