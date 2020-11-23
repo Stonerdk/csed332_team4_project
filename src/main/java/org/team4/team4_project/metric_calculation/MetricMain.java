75d9eaf451b30745f30120b30b46149223abe339
chaeyoon17 <chaeyoon17@postech.ac.kr>
2020-11-24 00:34:09 +0900

    MetricMain - add retrieveFiles method

--- src/main/java/org/team4/team4_project/metric_calculation/MetricMain.java
+++ src/main/java/org/team4/team4_project/metric_calculation/MetricMain.java
@@ -10,4 +10,42 @@ import org.eclipse.jdt.core.dom.*;
 
 public class MetricMain {
 
+    // retrieve all .java files in the directory and subdirectories.
+    public static List<String> retrieveFiles(String directory) {
+        List<String> Files = new ArrayList<String>();
+        File dir = new File(directory);
+
+
+        if(dir.isFile()){
+            if (dir.getName().endsWith((".java")))
+            {
+                Files.add(dir.getAbsolutePath());
+                return Files;
+            }
+        }
+
+        if (!dir.isDirectory())
+        {
+            System.out.println("The provided path is not a valid directory");
+            System.exit(1);
+        }
+
+
+
+        for (File file : dir.listFiles()) {
+            if(file.isDirectory())
+            {
+                Files.addAll(retrieveFiles(file.getAbsolutePath()));
+            }
+            if (file.getName().endsWith((".java")))
+            {
+                Files.add(file.getAbsolutePath());
+            }
+        }
+
+        return Files;
+    }
+
+
+
 }
