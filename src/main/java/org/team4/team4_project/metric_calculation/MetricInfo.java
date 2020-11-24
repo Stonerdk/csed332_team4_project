b3d6a157893d57922a0af8638a1a67c7412ff4d2
YuTaehyung <stephencurry@csed332.postech.ac.kr>
2020-11-24 14:53:49 +0900

    add set/get value functions of metrics in MetricInfo

--- src/main/java/org/team4/team4_project/metric_calculation/MetricInfo.java
+++ src/main/java/org/team4/team4_project/metric_calculation/MetricInfo.java
@@ -5,6 +5,11 @@ public class MetricInfo {
     private HashMap<String, Integer> operands = new HashMap<String, Integer>();
     private HashMap<String, Integer> operators = new HashMap<String, Integer>();
 
+    private int dist_operands;
+    private int dist_operators;
+    private int total_operands;
+    private int total_operators;
+
     //Halstead Metric
     private int halSteadVocabulary=0;
     private int halSteadProgLength=0;
@@ -24,5 +29,90 @@ public class MetricInfo {
 
     //Maintainability Index
     private double maintainabilityIndex;
-    
+
+
+    public void setByVisitor(ASTVisitorSearch visitor) {
+        operands = visitor.names;
+        operators = visitor.oprt;
+
+        dist_operators = operators.size();
+        dist_operands = operands.size();
+        total_operators = operators.values().stream().reduce(0, Integer::sum);
+        total_operands = operands.values().stream().reduce(0, Integer::sum);
+
+        CalHalstead calHalstead = new CalHalstead();
+        calHalstead.setParameters(dist_operators, dist_operands, total_operators, total_operands);
+
+        halSteadVocabulary = calHalstead.getVocabulary();
+        halSteadProgLength = calHalstead.getProglen();
+        halSteadCalProgLength = calHalstead.getCalcProgLen();
+        halSteadVolume = calHalstead.getVolume();
+        halSteadDifficulty = calHalstead.getDifficulty();
+        halSteadEffort = calHalstead.getEffort();
+        halSteadTimeRequired = calHalstead.getTimeReqProg();
+        halSteadNumDelBugs = calHalstead.getTimeDelBugs();
+
+
+        cyclomaticComplexity = visitor.cycloComplexity;
+
+        codeLen = visitor.codeLen;
+        commentLen = visitor.commentLen;
+
+        maintainabilityIndex = Math.max(0, 100 * (171.0 - 5.2 * Math.log(halSteadVolume) - 0.23 * cyclomaticComplexity - 16.2 * Math.log(codeLen) + 50.0 * Math.sin(Math.pow(2.4 * Math.toRadians(commentLen/codeLen), 0.5))) / 171.0);
+
+        return;
+    }
+
+    public int getDistOperands(){
+        return dist_operands;
+    }
+    public int getDistOperators(){
+        return dist_operators;
+    }
+    public int getTotalOperands(){
+        return total_operands;
+    }
+    public int getTotalOperators(){
+        return total_operators;
+    }
+
+    public int getHalsteadVocabulary(){
+        return halSteadVocabulary;
+    }
+    public int getHalsteadProgLength(){
+        return halSteadProgLength;
+    }
+    public double getHalsteadCalProgLength(){
+        return halSteadCalProgLength;
+    }
+    public double getHalsteadVolume(){
+        return halSteadVolume;
+    }
+    public double getHalsteadDifficulty(){
+        return halSteadDifficulty;
+    }
+    public double getHalsteadEffort(){
+        return halSteadEffort;
+    }
+    public double getHalsteadTimeRequired(){
+        return halSteadTimeRequired;
+    }
+    public double getHalsteadNumDelBugs(){
+        return halSteadNumDelBugs;
+    }
+
+    public int getCyclomaticComplexity(){
+        return cyclomaticComplexity;
+    }
+
+    public int getCodeLen(){
+        return codeLen;
+    }
+    public int getCommentLen(){
+        return commentLen;
+    }
+
+    public double getMaintainabilityIndex(){
+        return maintainabilityIndex;
+    }
 }
