7c1c081c15186785c295b16bf0bbd1621de57787
DESKTOP-MHPGQNN\yujin <jyj7913@postech.ac.kr>
2020-11-24 19:55:09 +0900

    Apply History Data to Graph

--- src/main/java/org/team4/team4_project/UI/MetricsWindow.java
+++ src/main/java/org/team4/team4_project/UI/MetricsWindow.java
@@ -1,6 +1,7 @@
 package org.team4.team4_project.UI;
 
 import org.team4.team4_project.history.HistoryData;
+import org.team4.team4_project.history.HistoryReader;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,7 +19,7 @@ public class MetricsWindow extends JFrame {
         int xCount;
         int nodeCount;
         ArrayList<HistoryData> historyList;
-        int score_java, score_python, score_cs;
+        String Type;
 
         GraphPanel() {
             setPreferredSize(new Dimension(640, 480));
@@ -32,43 +33,54 @@ public class MetricsWindow extends JFrame {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g.clearRect(0,0,getWidth(),getHeight());
-            g.drawLine(50,250,350,250);
-            for(int cnt = 1 ;cnt<11;cnt++)
+            g.drawLine(100,500,100 + 200*historyList.size(),500);
+            for(int cnt = 1; cnt < 11; cnt++)
             {
-                g.drawString(cnt *10 +"",25,255-20*cnt);
-                g.drawLine(50, 250-20*cnt, 350,250-20*cnt);
+                g.drawString(cnt *10 +"",50,510-40*cnt);
+                g.drawLine(100, 500-40*cnt, 100 + 200*historyList.size(),500-40*cnt);
             }
-            g.drawLine(50,20,50,250);
-            g.drawString("Java",100,270);
-            g.drawString("Python",200,270);
-            g.drawString("C#",300,270);
-            g.setColor(Color.BLUE);
-            if (score_java>0)
-                g.fillRect(110,250-score_java*2,10,score_java*2);
-            if(score_python>0)
-                g.fillRect(210,250-score_python*2,10,score_python*2);
-            if(score_cs>0)
-                g.fillRect(310,250-score_cs*2,10,score_cs*2);
+            g.drawLine(100,40,100,500);
+
+            //g.setColor(Color.BLUE);
+            int i = 1;
+            for(HistoryData h : historyList){
+                g.drawString(h.getCommitString(), 200 * i, 540);
+
+                g.fillRect(200*(i++) + 17, 500 -Integer.parseInt(String.valueOf(Math.round(h.getAttribute(Type))))*4,
+                        20, Integer.parseInt(String.valueOf(Math.round(h.getAttribute(Type))))*4);
+            }
+
         }
-        void setScores(int score_java, int score_python, int score_cs)
+        void setScores(ArrayList<HistoryData> HistoryList, int type)
         {
-            this.score_java=score_java;
-            this.score_python=score_python;
-            this.score_cs=score_cs;
+            historyList = HistoryList;
+            switch (type){
+                case 1:
+                    Type = "Halstead Complexity";
+                    break;
+                case 2:
+                    Type = "Cyclomatic Complexity";
+                    break;
+                case 3:
+                    Type = "Maintainability";
+                    break;
+            }
         }
     }
 
     private MetricsWindow () {
+        ArrayList<HistoryData> historyList = new HistoryReader().getHistoryList();;
+
         setTitle("Software Metrics Graph");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 800);
+        setSize(1300, 800);
 
         Container contentPane = getContentPane();
 
         JPanel p = new JPanel();
-        JButton Metric1 = new JButton("Metric1");
-        JButton Metric2 = new JButton("Metric2");
-        JButton Metric3 = new JButton("Metric3");
+        JButton Metric1 = new JButton("Halstead");
+        JButton Metric2 = new JButton("Cyclomatic");
+        JButton Metric3 = new JButton("Maintainability");
 
         p.add(Metric1);
         p.add(Metric2);
@@ -79,31 +91,9 @@ public class MetricsWindow extends JFrame {
         GraphPanel graphPanel = new GraphPanel();
         contentPane.add(graphPanel, BorderLayout.CENTER);
 
-        ArrayList<Integer> Metric1_ = new ArrayList<Integer>();
-        ArrayList<Integer> Metric2_ = new ArrayList<Integer>();
-        ArrayList<Integer> Metric3_ = new ArrayList<Integer>();
-
-        Metric1_.add(50);
-        Metric1_.add(34);
-        Metric1_.add(73);
-        Metric1_.add(92);
-        Metric1_.add(13);
-
-        Metric2_.add(25);
-        Metric2_.add(46);
-        Metric2_.add(14);
-        Metric2_.add(86);
-        Metric2_.add(94);
-
-        Metric3_.add(78);
-        Metric3_.add(36);
-        Metric3_.add(95);
-        Metric3_.add(67);
-        Metric3_.add(32);
-
-        Metric1.addActionListener(new DrawActionListener(50, 34, 73, graphPanel));
-        Metric2.addActionListener(new DrawActionListener(25,46, 14, graphPanel));
-        Metric3.addActionListener(new DrawActionListener(78,36,95, graphPanel));
+        Metric1.addActionListener(new DrawActionListener(historyList, graphPanel, 1));
+        Metric2.addActionListener(new DrawActionListener(historyList, graphPanel, 2));
+        Metric3.addActionListener(new DrawActionListener(historyList, graphPanel, 3));
 
     }
 
@@ -116,23 +106,20 @@ public class MetricsWindow extends JFrame {
 
     class DrawActionListener implements ActionListener
     {
-        int text1,text2,text3;
+        ArrayList<HistoryData> HistoryList;
         GraphPanel drawingPanel;
-        DrawActionListener(int text1, int text2, int text3, GraphPanel drawingPanel)
+        int type;
+        DrawActionListener(ArrayList<HistoryData> historylist, GraphPanel drawingPanel, int type)
         {
-            this.text1=text1;
-            this.text2=text2;
-            this.text3=text3;
+            HistoryList = historylist;
+            this.type = type;
             this.drawingPanel = drawingPanel;
         }
         public void actionPerformed(ActionEvent e)
         {
             try
             {
-                int korean = text1;
-                int english = text2;
-                int math = text3;
-                drawingPanel.setScores(korean, english, math);
+                drawingPanel.setScores(HistoryList, type);
                 drawingPanel.repaint();
             }
             catch (NumberFormatException nfe){
