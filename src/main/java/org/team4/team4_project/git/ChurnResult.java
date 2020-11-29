1dff744c9c47ff94ed245f3ad90e8c88b08ddea6
KimEoJin1 <kimuj7017@postech.ac.kr>
2020-11-29 17:44:12 +0900

    code complete to calculate metrics for each commit

--- src/main/java/org/team4/team4_project/git/ChurnResult.java
+++ src/main/java/org/team4/team4_project/git/ChurnResult.java
@@ -1,9 +1,15 @@
 package org.team4.team4_project.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChurnResult {
 
-    private int linesAdded = 0;
-    private int linesDeleted = 0;
+    private int linesAdded = 0; // 추가 된 코드 줄 수
+    private int linesDeleted = 0; // 삭제 된 코드 줄 수
+    private String code = ""; // 해당 commit의 code string
+
+    private List<String> codelist = new ArrayList<String>(); // line 별로 code 저장하는 list
 
     public int getTotalChurn() {
         return getLinesAdded() + getLinesDeleted();
@@ -25,4 +31,11 @@ public class ChurnResult {
         linesDeleted++;
     }
 
+    public String getcode() { return code;}
+
+    public void setcode(String c) {code = c;}
+
+    public List<String> getcodelist() { return codelist;}
+
+    public void setcodelist(List<String> s) { codelist = s;}
 }
--- src/main/java/org/team4/team4_project/git/CodeChurn.java
+++ src/main/java/org/team4/team4_project/git/CodeChurn.java
@@ -1,4 +1,5 @@
 package org.team4.team4_project.git;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.LogCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -14,6 +15,7 @@ import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
 
@@ -46,16 +48,11 @@ public class CodeChurn {
         return this;
     }
 
-    public CodeChurn add(AnyObjectId start) throws IncorrectObjectTypeException, MissingObjectException {
-        log.add(start);
-        return this;
-    }
-
     public List<ChurnResult> calc() throws GitAPIException, IOException {
         List<ChurnResult> results = new ArrayList<ChurnResult>();
+        List<String> differences = new ArrayList<String>();
         Iterable<RevCommit> commits = log.all().call();
         for (RevCommit commit : commits) {
-        //RevCommit commit = commits.iterator().next();
             ChurnResult result = new ChurnResult();
             RevCommit[] parents = commit.getParents();
             for (RevCommit parent : parents) {
@@ -66,8 +63,8 @@ public class CodeChurn {
                 formatter.setPathFilter(PathFilter.create(path));
                 formatter.format(parent, commit);
                 String difference = stream.toString();
+                differences.add(difference);
                 Scanner scanner = new Scanner(difference);
-                //System.out.println(difference);
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
                     if (line.startsWith("+") && !line.startsWith("+++")) {
@@ -78,24 +75,23 @@ public class CodeChurn {
                 }
             }
             if (parents.length == 0){
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                DiffFormatter formatter = new DiffFormatter(stream);
+                formatter.setRepository(repo);
+                formatter.setContext(0);
+                formatter.setPathFilter(PathFilter.create(path));
+
                 ObjectReader reader = repo.newObjectReader();
                 RevWalk rw = new RevWalk(reader);
                 RevTree rtree = rw.parseTree(commit);
                 CanonicalTreeParser parser = new CanonicalTreeParser();
                 parser.reset(reader, rtree);
                 AbstractTreeIterator iterator = (AbstractTreeIterator)parser;
-                AbstractTreeIterator empty = iterator.createEmptyTreeIterator();
-                TreeWalk walk = new TreeWalk(reader);
-                walk.addTree(empty);
-                walk.addTree(iterator);
-                List<DiffEntry> diffs = DiffEntry.scan(walk);
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                DiffFormatter formatter = new DiffFormatter(stream);
-                formatter.setRepository(repo);
-                formatter.setContext(0);
-                formatter.setPathFilter(PathFilter.create(path));
+                AbstractTreeIterator empty = new EmptyTreeIterator();
+                List<DiffEntry> diffs = formatter.scan(empty, iterator);
                 formatter.format(diffs);
                 String difference = stream.toString();
+                differences.add(difference);
                 Scanner scanner = new Scanner(difference);
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
@@ -109,9 +105,99 @@ public class CodeChurn {
             results.add(result);
         }
 
+        results = completeCode(results, differences);
+        return results;
+    }
+
+    public List<ChurnResult> completeCode(List<ChurnResult> results, List<String> differences) {
+        List<String> codes = new ArrayList<String>();
+        Integer pre = 1;
+        for (int i=0; i<differences.size(); i++){
+                if (i>0) {
+                    codes = results.get(results.size() - i).getcodelist();
+                    String difference = differences.get(differences.size() - 1 - i);
+                    Scanner scanner = new Scanner(difference);
+                    int deletenumlines = 0;
+                    int deletestartline = 1;
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        if (line.startsWith("-") && !line.startsWith("---")) {
+                            codes.remove(deletestartline - deletenumlines);
+                            deletestartline++;
+                        } else if (line.startsWith("@@")) {
+                            String[] split = line.split(" ");
+                            String[] split3 = split[1].split(",");
+                            if (split3.length > 1) {
+                                deletenumlines = deletenumlines + Integer.valueOf(split3[1]);
+                            }
+                            else {
+                                deletenumlines++;
+                            }
+                            deletestartline = Integer.valueOf(split3[0]) * -1;
+                        }
+                    }
+
+
+                    Scanner scanner2 = new Scanner(difference);
+                    int addnumLines = 1;
+                    int addstartLine = 1;
+                    while (scanner2.hasNextLine()) {
+                        String line = scanner2.nextLine();
+                        if (line.startsWith("+") && !line.startsWith("+++")) {
+                            String code = line.substring(1, line.length());
+                            codes.set(addstartLine - 1, code);
+                            addstartLine++;
+                        }
+                        else if (line.startsWith("@@")) {
+                            String[] split = line.split(" ");
+                            String[] split2 = split[2].split(",");
+                            if (split2.length > 1) {
+                                addnumLines = Integer.valueOf(split2[1]);
+                            }
+                            addstartLine = Integer.valueOf(split2[0]);
+                            codes = pushString(codes, addnumLines, addstartLine);
+                            System.out.println("dsatadt");
+                        }
+                    }
+                }
+
+                else {
+                    String difference = differences.get(differences.size() - 1 - i);
+                    Scanner scanner = new Scanner(difference);
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        if (line.startsWith("+") && !line.startsWith("+++")) {
+                            String code = line.substring(1, line.length());
+                            codes.add(code);
+                        }
+                    }
+                }
+
+            String code = "";
+            for (int j=0; j<codes.size(); j++){
+                if(j == codes.size()-1)
+                    code = code + codes.get(j);
+                else
+                    code = code + codes.get(j) + "\n";
+            }
+            results.get(results.size()-1-i).setcode(code);
+            results.get(results.size()-1-i).setcodelist(codes);
+        }
+
 
         return results;
     }
 
+    public List<String> pushString (List<String> codes, int addnumLines, int addstartLine){
+        int codessize = codes.size();
+        for (int i =0; i<addnumLines; i++){
+            codes.add("");
+        }
 
+        for (int i = codessize-1; i >= addstartLine - 1 ; i--){
+            codes.set(i + addnumLines, codes.get(i));
+        }
+
+        return codes;
+    }
 }
