18ca7e84d7b981b6b75b197af7072676383236ca
YuTaehyung <stephencurry@csed332.postech.ac.kr>
2020-12-07 16:34:07 +0900

    changed class name (calHalstead to HalsteadMetric) / changed public class value to private

--- src/test/java/org/team4/team4_project/swmetric/HalsteadCalculationTest.java
+++ src/test/java/org/team4/team4_project/swmetric/HalsteadCalculationTest.java
@@ -3,65 +3,42 @@ package org.team4.team4_project.swmetric;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.team4.team4_project.metric_calculation.CalHalstead;
+import org.team4.team4_project.metric_calculation.HalsteadMetric;
 
 public class HalsteadCalculationTest {
     @Test
     public void testgetVocabulary() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        assertEquals(17, calHalstead.getVocabulary());
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(17, halsteadMetric.getVocabulary());
     }
-
     @Test
     public void testgetProglen() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        assertEquals(31, calHalstead.getProglen());
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(31, halsteadMetric.getProglen());
     }
-
     @Test
     public void testgetVolume() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        System.out.println("getVolume= "+ (double)Math.round(calHalstead.getVolume()));
-        assertEquals(127, Math.round(calHalstead.getVolume()));
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(127, Math.round(halsteadMetric.getVolume()));
     }
-
-
     @Test
     public void testgetDifficulty() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        System.out.println("getDifficulty= "+ (double)Math.round(calHalstead.getDifficulty()));
-        assertEquals(11, Math.round(calHalstead.getDifficulty()));
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(11, Math.round(halsteadMetric.getDifficulty()));
     }
-
-
     @Test
     public void testgetEffort() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        System.out.println("getEffort= "+ (double)Math.round(calHalstead.getEffort()));
-        assertEquals(1358, Math.round(calHalstead.getEffort()));
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(1358, Math.round(halsteadMetric.getEffort()));
     }
-
-
-
     @Test
     public void testgetTimeReqProg() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        System.out.println("getTimeReqProg= "+ (double)Math.round(calHalstead.getTimeReqProg()));
-        assertEquals(75, Math.round(calHalstead.getTimeReqProg()));
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(75, Math.round(halsteadMetric.getTimeReqProg()));
     }
-
-
     @Test
     public void testgetTimeDelBugs() {
-        CalHalstead calHalstead = new CalHalstead();
-        calHalstead.setParameters(10, 7, 16, 15);
-        System.out.println("getTimeDelBugs= "+ (double)Math.round(calHalstead.getTimeDelBugs()));
-        assertEquals(0, Math.round(calHalstead.getTimeDelBugs()));
+        HalsteadMetric halsteadMetric = new HalsteadMetric(10, 7, 16, 15);
+        assertEquals(0, Math.round(halsteadMetric.getTimeDelBugs()));
     }
 }
