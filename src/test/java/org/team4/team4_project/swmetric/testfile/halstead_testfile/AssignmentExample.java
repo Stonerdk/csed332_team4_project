39b9f2ee264e25d9785abd343dedfeff1e679d4b
YuTaehyung <stephencurry@csed332.postech.ac.kr>
2020-12-13 19:56:40 +0900

    Unit tests for Halstead Tests added & All Passed

--- src/test/java/org/team4/team4_project/swmetric/testfile/halstead_testfile/AssignmentExample.java
+++ src/test/java/org/team4/team4_project/swmetric/testfile/halstead_testfile/AssignmentExample.java
@@ -3,19 +3,8 @@ package org.team4.team4_project.swmetric.testfile.halstead_testfile;
 public class AssignmentExample {
     public static void main(String args[]) {
         int a = 2;
-        int b = 2;
+        int b = 1;
 
-        String must = null;
-        must = "asd" + "bbb";
-
-        char aaa = 'A';
-        aaa = 'B';
-
-        boolean c = true;
-        c = !c;
-        a = ~a;
-
-        /*
         a += b;
         a -= b;
         a *= b;
@@ -29,18 +18,20 @@ public class AssignmentExample {
         a >>= b;
         a >>>= b;
 
-
-        if(a == 2 || b != 1) {
-            System.out.println(1);
-        }
-
-        */
+    }
+}
         /*
         if(a == 2 || b != 1) {
             System.out.println(1);
         }
 
-         */
+        /*
+        int a = 2;
+        int b = 2;
 
-    }
-}
+        String must = null;
+        must = "asd" + "bbb";
+
+        char aaa = 'A';
+        aaa = 'B';
+         */
