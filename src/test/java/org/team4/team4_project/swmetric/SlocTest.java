d25d9b9492d53e703c32e222f3189e8b3172a697
YuTaehyung <stephencurry@csed332.postech.ac.kr>
2020-12-14 16:37:13 +0900

    Added unit test for number of loop and SLOC & Test all passed

---
+++ src/test/java/org/team4/team4_project/swmetric/SlocTest.java
@@ -0,0 +1,44 @@
+package org.team4.team4_project.swmetric;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.team4.team4_project.metric_calculation.ASTVisitorSearch;
+import org.team4.team4_project.metric_calculation.MetricInfo;
+import org.team4.team4_project.metric_calculation.MetricMain;
+
+import java.io.IOException;
+
+public class SlocTest {
+    MetricMain metricMain = new MetricMain();
+
+    @Test
+    public void testcodeline1() throws IOException {
+        String filename = null;
+        filename = "src\\test\\java\\org\\team4\\team4_project\\swmetric\\testfile\\cyclomatic_testfile\\IfElseExample.java";
+        char[] fileread = metricMain.ReadFileToCharArray(filename);
+        ASTVisitorSearch ASTVisitorFile;
+        ASTVisitorFile = metricMain.parse(fileread, 1);
+
+        MetricInfo metricInfo = new MetricInfo();
+        metricInfo.setByVisitor(ASTVisitorFile);
+
+        //System.out.println("Cyclometic complexity in this .java file = " + metricInfo.getCyclomaticComplexity());
+        Assert.assertEquals(19, metricInfo.getCodeLen());
+    }
+
+    public void testcodeline2() throws IOException {
+        String filename = null;
+        filename = "src\\test\\java\\org\\team4\\team4_project\\swmetric\\testfile\\MethodDeclarationExample.java";
+        char[] fileread = metricMain.ReadFileToCharArray(filename);
+        ASTVisitorSearch ASTVisitorFile;
+        ASTVisitorFile = metricMain.parse(fileread, 1);
+
+        MetricInfo metricInfo = new MetricInfo();
+        metricInfo.setByVisitor(ASTVisitorFile);
+
+        //System.out.println("Cyclometic complexity in this .java file = " + metricInfo.getCyclomaticComplexity());
+        Assert.assertEquals(24, metricInfo.getCodeLen());
+    }
+
+}
+
--- src/test/java/org/team4/team4_project/swmetric/testfile/cyclomatic_testfile/ForLoopExample.java
+++ src/test/java/org/team4/team4_project/swmetric/testfile/cyclomatic_testfile/ForLoopExample.java
@@ -5,6 +5,9 @@ public class ForLoopExample {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 0; i < 10; i++) {
+            for(int j = 0; j <10; j++) {
+                System.out.println(j);
+            }
             sum += i;
         }
         for (int i = 0; i < 10; i++) {
